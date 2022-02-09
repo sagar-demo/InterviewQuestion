88be80a2f672d59c5ae0f98e3be60f9a85350fc5
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-02-09 08:17:17 -0500

    Add files via upload

---
+++ Positive_or_negative.py
@@ -0,0 +1,19 @@
+# Question was
+# x=int(input("Enter a number:"))
+#
+# if x<0:
+#     print("This is Negative Integer")
+# elif x>=0:
+#     print("This is Positive Integer")
+# else:
+#     print("Zero")
+# Using the Nested  if-else Statements
+
+x=5
+if x>=0:
+    if x==0:
+        print("Zero number")
+    else:
+        print("Positive number")
+else:
+    print("The number is Negative")
